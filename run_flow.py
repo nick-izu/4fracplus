a523c0e98693ff5b680a07a8cc15f182f0e202e1
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-03-14 09:15:03 +0100

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -1,4 +1,3 @@
-!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep 10 12:04:03 2020
@@ -22,16 +21,7 @@ DFN = create_dfn()
 DFN.make_working_directory()
 DFN.check_input()
 DFN.create_network()
-DFN.mesh_network(visual_mode=True)
-
-#octree
-
-DFN.set_flow_solver("PFLOTRAN")
-DFN.inp_file = "octree_dfn.inp"
-DFN.map_to_continuum(l=5,orl=3)
-DFN.upscale(mat_perm=1e-15,mat_por=0.01)
-DFN.zone2ex(uge_file='full_mesh.uge',zone_file='all')
-
+DFN.mesh_network(visual_mode=False)
 
 ##dfnFlow
 DFN.pflotran()
