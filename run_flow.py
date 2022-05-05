80f7edf67c92e5a01f5f172382cbebb9fa737273
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-05-05 10:29:58 +0200

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -23,6 +23,28 @@ DFN.check_input()
 DFN.create_network()
 DFN.mesh_network(visual_mode=False)
 
+# assign hydraulic properties by family
+
+b[0]= 1e-3
+b[1]= 1e-3
+b[2]= 1e-3
+b[3]= 5e-4
+
+variable = "transmissivity"
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.8}
+b4,perm2,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=None)
+
+T = T0 + T1 + T2 + T3 + T4
+
+b = b0 + b1 + b2 + b3+ b4
+
+perm = perm0 + perm1 + perm2 + perm3 + perm4
+DFN.dump_hydraulic_values(b,perm,T)
+
+# Add transmissivity values to the mesh for visualization
+DFN.add_variable_to_mesh("trans,", "transmissivity.dat", "full_mesh.inp")
+
 ##dfnFlow
 #DFN.pflotran()
 #DFN.parse.pflotran_vtk_python()
