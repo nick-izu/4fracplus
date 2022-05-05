0f7ead79c3b9d702fb908062469f3f7feafa8e4c
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-05-05 11:24:35 +0200

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -25,21 +25,38 @@ DFN.mesh_network(visual_mode=False)
 
 # assign hydraulic properties by family
 
-b[0]= 1e-3
-b[1]= 1e-3
-b[2]= 1e-3
-b[3]= 5e-4
-
 variable = "transmissivity"
+function = "constant"
+params = {"mu":1*10**-3}
+b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+
+function = "constant"
+params = {"mu":1*10**-3}
+b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
+
+function = "constant"
+params = {"mu":1*10**-3}
+b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
+
+function = "constant"
+params = {"mu":5*10**-4}
+b4,perm4,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=4)
+
 function = "correlated"
 params = {"alpha":2.2*10**-9, "beta":0.8}
-b4,perm4,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=None)
+b5,perm5,T5 = DFN.generate_hydraulic_values(variable,function,params,family_id=None)
 
-T = T0 + T1 + T2 + T3 + T4
 
-b = b0 + b1 + b2 + b3+ b4
+T = T1 + T2 + T3 + T4 + T5
 
-perm = perm0 + perm1 + perm2 + perm3 + perm4
+b = b1 + b2 + b3+ b4 + b5
+
+b[1]= 1e-3
+b[2]= 1e-3
+b[3]= 1e-3
+b[4]= 5e-4
+perm = perm1 + perm2 + perm3 + perm4 + perm5
+
 DFN.dump_hydraulic_values(b,perm,T)
 
 # Add transmissivity values to the mesh for visualization
