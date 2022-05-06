5835a783b4f889d8b66251820797c4c72caffb0e
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-05-06 16:49:35 +0200

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -25,37 +25,43 @@ DFN.mesh_network(visual_mode=False)
 
 # assign hydraulic properties by family
 
-variable = "transmissivity"
+variable = "aperture"
 function = "constant"
 params = {"mu":1*10**-3}
-b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=0)
 
 function = "constant"
 params = {"mu":1*10**-3}
-b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
+b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=0)
 
 function = "constant"
 params = {"mu":1*10**-3}
-b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
+b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=0)
 
 function = "constant"
 params = {"mu":5*10**-4}
-b4,perm4,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=4)
+b4,perm4,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=0)
+
+variable = "transmissivity"
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.8}
+b5,perm5,T5 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
 
 function = "correlated"
 params = {"alpha":2.2*10**-9, "beta":0.8}
-b5,perm5,T5 = DFN.generate_hydraulic_values(variable,function,params,family_id=None)
+b6,perm6,T6 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
+
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.8}
+b7,perm7,T7 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
 
 
-T = T1 + T2 + T3 + T4 + T5
 
-b = b1 + b2 + b3+ b4 + b5
+T = T1 + T2 + T3 + T4 + T5 + T6 + T7
 
-b[1]= 1e-3
-b[2]= 1e-3
-b[3]= 1e-3
-b[4]= 5e-4
-perm = perm1 + perm2 + perm3 + perm4 + perm5
+b = b1 + b2 + b3+ b4 + b5 + b6 + b7
+
+perm = perm1 + perm2 + perm3 + perm4 + perm5 + perm6 + perm7
 
 DFN.dump_hydraulic_values(b,perm,T)
 
